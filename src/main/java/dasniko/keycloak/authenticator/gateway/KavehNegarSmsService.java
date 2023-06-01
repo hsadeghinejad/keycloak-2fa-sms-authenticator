a7db11498d955bc67cdeaa2c3c90f04bdea75201
hamed <h.sadeghynejad@gmail.com>
2023-06-01 14:57:19 +0330

    add kavenegar functionality

---
+++ src/main/java/dasniko/keycloak/authenticator/gateway/KavehNegarSmsService.java
@@ -0,0 +1,49 @@
+import okhttp3.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class KavehNegarSmsService {
+
+    private static final String API_URL = "https://api.kavehnegar.com/v1/{API_KEY}/verify/lookup.json";
+    private final String apiKey;
+
+    private final OkHttpClient httpClient = new OkHttpClient();
+
+    public KavehNegarSmsService() {
+        Properties prop = new Properties();
+        try (InputStream input = KavehNegarSmsService.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find config.properties");
+            }
+            //load a properties file from class path
+            prop.load(input);
+
+            //get the property value
+            apiKey = prop.getProperty("api_key");
+        } catch (IOException ex) {
+            throw new RuntimeException("Error reading from config.properties", ex);
+        }
+    }
+
+    public void send(String phoneNumber, String token) throws IOException {
+        send(phoneNumber, token, "password-change-3");
+    }
+
+    public void send(String phoneNumber, String token, String template) throws IOException {
+        RequestBody formBody = new FormBody.Builder()
+                .add("receptor", phoneNumber)
+                .add("token", token)
+                .add("template", template)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(API_URL.replace("{API_KEY}", apiKey))
+                .post(formBody)
+                .build();
+
+        Response response = httpClient.newCall(request).execute();
+
+        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+    }
+}
--- src/main/java/dasniko/keycloak/authenticator/gateway/SmsServiceFactory.java
+++ src/main/java/dasniko/keycloak/authenticator/gateway/SmsServiceFactory.java
@@ -4,20 +4,23 @@ import org.jboss.logging.Logger;
 
 import java.util.Map;
 
-/**
- * @author Niko Köbler, https://www.n-k.de, @dasniko
- */
 public class SmsServiceFactory {
 
-	private static final Logger LOG = Logger.getLogger(SmsServiceFactory.class);
+    private static final Logger LOG = Logger.getLogger(SmsServiceFactory.class);
 
-	public static SmsService get(Map<String, String> config) {
-		if (Boolean.parseBoolean(config.getOrDefault("simulation", "false"))) {
-			return (phoneNumber, message) ->
-				LOG.warn(String.format("***** SIMULATION MODE ***** Would send SMS to %s with text: %s", phoneNumber, message));
-		} else {
-			return new AwsSmsService(config);
-		}
-	}
-
+    public static SmsService get(Map<String, String> config) {
+        if (Boolean.parseBoolean(config.getOrDefault("simulation", "false"))) {
+            return (phoneNumber, message) ->
+                LOG.warn(String.format("***** SIMULATION MODE ***** Would send SMS to %s with text: %s", phoneNumber, message));
+        } else {
+            String serviceType = config.getOrDefault("sms_service", "aws");
+            if ("kavehnegar".equals(serviceType)) {
+                return new KavehNegarSmsService(config);
+            } else if ("aws".equals(serviceType)) {
+                return new AwsSmsService(config);
+            } else {
+                throw new IllegalArgumentException("Unsupported sms_service: " + serviceType);
+            }
+        }
+    }
 }
