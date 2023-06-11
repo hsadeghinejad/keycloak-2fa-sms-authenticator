a3afa9325cfdb6e881d63073e6bb421b42d77040
hamed <h.sadeghynejad@gmail.com>
2023-06-11 14:39:41 +0330

    fix bug

--- src/main/java/dasniko/keycloak/authenticator/gateway/KavehNegarSmsService.java
+++ src/main/java/dasniko/keycloak/authenticator/gateway/KavehNegarSmsService.java
@@ -1,31 +1,39 @@
-package dasniko.keycloak.authenticator.gateway;
-
 import okhttp3.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import java.util.Map;
+public class KavehNegarSmsService {
 
-public class KavehNegarSmsService implements SmsService{
-
-    // private static final String API_URL = "https://api.kavehnegar.com/v1/{API_KEY}/verify/lookup.json";
     private static final String API_URL = "https://api.kavenegar.com/v1/{API-KEY}/sms/send.json";
     private final String apiKey;
 
     private final OkHttpClient httpClient = new OkHttpClient();
 
-    public KavehNegarSmsService(Map<String, String> config) {
-        apiKey = config.get("API_KEY");
-        if (apiKey == null) {
-            throw new RuntimeException("API_KEY environment variable is not set");
+    public KavehNegarSmsService() {
+        Properties prop = new Properties();
+        try (InputStream input = KavehNegarSmsService.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find config.properties");
+            }
+            //load a properties file from class path
+            prop.load(input);
+
+            //get the property value
+            apiKey = prop.getProperty("api_key");
+        } catch (IOException ex) {
+            throw new RuntimeException("Error reading from config.properties", ex);
         }
     }
 
-    public void send(String phoneNumber, String message) throws IOException {
+    public void send(String phoneNumber, String token) throws IOException {
+        send(phoneNumber, token, "password-change-3");
+    }
+
+    public void send(String phoneNumber, String token, String template) throws IOException {
         RequestBody formBody = new FormBody.Builder()
                 .add("receptor", phoneNumber)
-                .add("message", message)
+                .add("message", token)
                 .build();
 
         Request request = new Request.Builder()
