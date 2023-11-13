1a347de5de3b8e5c2034130150117fa2e9f784fe
Hamed Sadeghinejad <38876242+hsadeghinejad@users.noreply.github.com>
2023-11-13 11:36:40 +0330

    fix bugs

--- src/main/java/dasniko/keycloak/authenticator/SmsConstants.java
+++ src/main/java/dasniko/keycloak/authenticator/SmsConstants.java
@@ -7,6 +7,9 @@ public class SmsConstants {
 	public String CODE = "code";
 	public String CODE_LENGTH = "length";
 	public String CODE_TTL = "ttl";
+	public String API_KEY = "API_KEY";
+	public String SMS_SERVICE = "SMS_SERVICE";
+	public String SMS_TEMPLATE = "sms_template";
 	public String SENDER_ID = "senderId";
 	public String SIMULATION_MODE = "simulation";
 }
